fdb1d8be0ff31bb9d2548188426d4b5de505ca3f
testboy450 <testboy450@gmail.com>
2022-03-16 14:46:33 -0700

    comm:

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,43 +1,22 @@
 #include "main.h"
 
-
-
-/**
+/*
+ * main - prints _putchar
  *
- *  * main - prints _putchar
- *
- *   *
- *
- *    * Return: 0 (Success)
- *
- *     */
-
-
+ * Return: 0 (Success)
+ */
 
 int main(void)
-
 {
-
-		_putchar('_');
-
-			_putchar('p');
-
-				_putchar('u');
-
-					_putchar('t');
-
-						_putchar('c');
-
-							_putchar('h');
-
-								_putchar('a');
-
-									_putchar('r');
-
-										_putchar('\n');
-
-
-
-											return (0);
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
 
 }
