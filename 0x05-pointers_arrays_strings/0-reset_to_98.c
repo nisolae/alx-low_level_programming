28b5a38889bb8304167fbb2b873c7378691cddde
nisolae <testboy450@gmail.com>
2022-03-21 18:28:53 -0700

    swap

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * reset_to_98 - a function that takes a pointer
+ *  to int 98
+ *  @c: takes in a pointer
+ *  Return: (0)
+ */
+
+void reset_to_98(int *c)
+{
+	*c = 98;
+}
