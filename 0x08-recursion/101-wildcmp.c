41dc54c84347c6055ec0b2e7fe3656384ddecb86
nisolae <testboy450@gmail.com>
2022-03-31 17:51:14 -0700

    wildcmp

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,25 +1,32 @@
-int _strstr(char *h, char *n)
+#include "main.h"
+/**
+ * str_checker - check if two strings are identical.
+ * @s1: string_1 base address.
+ * @s2: string_2 base address.
+ * @i: left index.
+ * @j: special index. (joker)
+ * Return: 1 if s is palindrome, 0 otherwise.
+ */
+int str_checker(char *s1, char *s2, int i, int j)
 {
-  if (*n == *h && *n != '\0')
-    return (_strstr(h++, n++));
-  if (*n == '*')
-    return (_strstr(h, n++));
-  if (*n == '\0')
-    return (1);
+	if (s1[i] == '\0' && s2[j] == '\0')
+		return (1);
+	if (s1[i] == s2[j])
+		return (str_checker(s1, s2, i + 1, j + 1));
+	if (s1[i] == '\0' && s2[j] == '*')
+		return (str_checker(s1, s2, i, j + 1));
+	if (s2[j] == '*')
+		return (str_checker(s1, s2, i + 1, j) || str_checker(s1, s2, i, j + 1));
+	return (0);
 }
-
-int wilddos(char *s1, char *s2)
-{
-  if (*s2 == *s1 || *s2 == '*')
-    return (_strstr(s1, s2));
-  else
-    return (0);
-}
-
+/**
+ * wildcmp - check if strings could be considered identical
+ * @s1: base address for string.
+ * @s2: base address for string.
+ *
+ * Return: 1 if are considered identical.
+ */
 int wildcmp(char *s1, char *s2)
 {
-    if (*s1 != '\0')
-        return (wilddos(s1++, s2++));
-    else 
-        return (0);
+	return (str_checker(s1, s2, 0, 0));
 }
